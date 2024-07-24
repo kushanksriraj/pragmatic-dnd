df83516c8ba2f2058b50bc090c355b0f667de2c7
kushanksriraj <kushanksriraj@gmail.com>
2024-07-24 15:20:59 +0530

    repro issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '@atlaskit/css-reset';
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactDOM18 from "react-dom/client";
+import "@atlaskit/css-reset";
 
-import Example from './example';
+import Example from "./example";
 
-ReactDOM.render(
-  <Example />,
-  document.getElementById('root')
-);
+// The cursor position works fine
+ReactDOM.render(<Example />, document.getElementById("root"));
+
+// The cursor position always goes to top left after dragging
+// ReactDOM18.createRoot(document.getElementById("root")!).render(<Example />);
